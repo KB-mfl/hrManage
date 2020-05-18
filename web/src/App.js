1e1d19975b3235f12bb697f13e68c233d9083111
zziqiang <z2206071258@163.com>
2020-05-18 22:14:38 +0800

    add dep

--- web/src/App.js
+++ web/src/App.js
@@ -1,17 +1,14 @@
-import React from 'react';
+import React from "react";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       visiable: true,
-    }
+    };
   }
-  render(){
-    return (
-      <div>
-        asxas
-      </div>
-    )
+
+  render() {
+    return <div>app</div>;
   }
 }
