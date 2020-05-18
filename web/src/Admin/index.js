f1c7ba202d9b1d62e555ac7b1fd3091c70cfc33f
zziqiang <z2206071258@163.com>
2020-05-18 22:35:50 +0800

    add role

---
+++ web/src/Admin/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class Admin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visiable: null,
+    };
+  }
+
+  render() {
+    return <div>admin</div>;
+  }
+}
---
+++ web/src/User/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class User extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visiable: null,
+    };
+  }
+
+  render() {
+    return <div>user</div>;
+  }
+}
