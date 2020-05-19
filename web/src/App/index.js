cd3b20e3e2835e2c6877ee34ac664fec7647a309
zhuziqiang03 <zhuziqiang03@meituan.com>
2020-05-19 21:04:24 +0800

    add theme

--- web/src/App/index.js
+++ web/src/App/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button, Modal } from "antd";
-import "./index.css";
+import "./index.less";
 
 export default class App extends React.Component {
   constructor(props) {
